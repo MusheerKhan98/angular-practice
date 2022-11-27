c039d0299adc0977c518f632f9798d09498d2cce
MusheerKhan98 <86644146+MusheerKhan98@users.noreply.github.com>
2022-11-27 14:18:51 +0530

    4 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, ServersComponent,WarningAlertComponent,SuccessAlertComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
